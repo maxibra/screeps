f786f5cf529d024dce7c7dbf7de64ed62d389137
Max Braver <mbraver@salesforce.com>
2016-11-23 18:42:46 +0200

    first commit. Tutorial3

---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by mbraver on 23/11/2016.
+ */
+var roleHarvester = require('role.harvester');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
