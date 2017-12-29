7bc8b744499770d88854273a40cd149f3655b27d
Max Braver <mbraver@salesforce.com>
2017-12-29 19:19:17 +0200

    tower repair defence if amount of creeps greater than 14

--- role.tower.js
+++ role.tower.js
@@ -6,20 +6,20 @@ var my_room = Game.rooms[global_vars.room_name];
 
 var RoleTower;
 RoleTower = {
-    run: function (current_tower_id) {
+    run: function (current_tower_id, creeps_amount) {
         /*
          current_tower    -   Object of current tower
          Return:    amount of missing energy
          */
         let target2repair;
         let current_tower = Game.getObjectById(current_tower_id);
-        if (Game.spawns[spawn_name].memory.general.status == 'war') {
+        if (Game.spawns[spawn_name].memory.general.status == 'war' || creeps_amount >= 14 ) {
             target2repair = Game.getObjectById(Game.rooms[global_vars.room_name].memory.target_repair_defence);
-            console.log('[INFO] (RoleTower.run) it"s WAR: Repair WALL');
+            console.log('[INFO] (RoleTower.run) it"s WAR: Repair DEFENCE');
         } else {
             let targets2repair = (Game.rooms[global_vars.room_name].memory.towers.road2repair_id) ? Game.getObjectById(Game.rooms[global_vars.room_name].memory.towers.road2repair_id) :
                 Game.rooms[global_vars.room_name].find(FIND_STRUCTURES,
-                    {filter: object => (object.structureType === STRUCTURE_ROAD && (object.hits < object.hitsMax))});
+                    {filter: object => (object.structureType === STRUCTURE_ROAD && (object.hits/object.hitsMax < 0.85))});
             //console.log('[DEBUG] (RoleTower.run): Road X: ' + road2repair.pos.x + '; Road y: ' + road2repair.pos.y);
             target2repair = (targets2repair) ? targets2repair[0] : Game.rooms[global_vars.room_name].memory.target_repair_defence;
             console.log('[DEBUG] (RoleTower.run) Roads to repear: ' + targets2repair.length);
