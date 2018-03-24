eaa1a3e307cba247c426492c9002d1c1f5a715a8
Max Braver <mbraver@salesforce.com>
2018-03-24 10:46:14 +0300

    disable creation of max_new

--- creep_helpers.js
+++ creep_helpers.js
@@ -42,18 +42,19 @@ var creep_helpers = {
         let add_body = creep_body.general.add;
 
         creep_name = '';
-        if (room_name !== 'E38N48') {
-            let new_memory = {role: 'harvest', harvester_type: 'source', target_id: '59f1a59182100e1594f3eb89', stuck: 0};
-            for (let i=1; i<=new_room_creeps; i++) {
-                current_new_name = 'max_new-' + i;
-                // console.log('[DEBUG] (create_creep): CURRENT NAME: ' + current_new_name)
-                if ( Object.keys(Game.creeps).indexOf(current_new_name) === -1 ) {
-                    creep_name = current_new_name;
-                    creep_memory = new_memory;
-                    break;
-                }
-            }
-        }
+        // creation of additional creeps for expansion
+        // if (room_name !== 'E38N48') {
+        //     let new_memory = {role: 'harvest', harvester_type: 'source', target_id: '59f1a59182100e1594f3eb89', stuck: 0};
+        //     for (let i=1; i<=new_room_creeps; i++) {
+        //         current_new_name = 'max_new-' + i;
+        //         // console.log('[DEBUG] (create_creep): CURRENT NAME: ' + current_new_name)
+        //         if ( Object.keys(Game.creeps).indexOf(current_new_name) === -1 ) {
+        //             creep_name = current_new_name;
+        //             creep_memory = new_memory;
+        //             break;
+        //         }
+        //     }
+        // }
 
         // **** LOG
         // console.log('[DEBUG] (create_creep)[' + spawn_name + ']: Creeps: ' +  units[room_name].total + '; Must Be: ' + room_vars.screeps_max_amount[room_vars.status] + '; SPAWING: ' + my_spawn.spawning + '; no needed a New: ' + (creep_name === ''));
