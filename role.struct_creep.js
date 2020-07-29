ec27a464420ef5ecbbcbe19f238d18f1006809d0
maxibra <max.braver@gmail.com>
2020-07-29 17:50:55 +0300

    log

--- role.struct_creep.js
+++ role.struct_creep.js
@@ -1256,6 +1256,7 @@ var structCreep = {
                     // else hostile_creeps_ids = my_room.memory.targets.hostile.work;
                     
                     let hostile_creeps = []
+                    console.log('[DEBUG] (structCreep.attacker)[' + creep.name + '] hostile_creeps_ids: ' + hostile_creeps_ids);
                     for (let i of hostile_creeps_ids) {
                         hostile_creeps.push(Game.getObjectById(i))
                     }
